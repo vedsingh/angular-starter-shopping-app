fa1eaa7ceaa522922188057a6d4c8f8e3c59bfb8
vedsingh <ved.singh@gmail.com>
2021-05-25 13:49:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-u7hikk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-u7hikk)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    alert(
+      'Your order has been submitted by ' +
+        this.checkoutForm.value.name +
+        ' at address ' +
+        this.checkoutForm.value.address
+    );
+    this.checkoutForm.reset();
+  }
+
+  ngOnInit() {}
+}
